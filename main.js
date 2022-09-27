c9d77fc0a8bd9ae6f5e2c346de72ea31c76d7b14
TAI VAN <taivannho5a@gmail.com>
2022-09-27 23:18:46 +0700

    Viết logic đổi màu realtime

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
+const appElement = document.querySelector('#app');
 const colorItems = document.querySelectorAll('.color-item');
+// State để lưu màu
+const state = [];
 
-colorItems.forEach(colorItem => {
-    colorItem.onchange = (e) => {
-        console.log(123);
+colorItems.forEach((colorItem, index) => {
+    state[index] = colorItem.value;
+    
+    colorItem.oninput = (e) => {
+        const newValue = e.target.value;
+
+        colorItem.value = newValue;
+        state[index] = newValue;
+
+        if (index === 0) {
+            appElement.style.backgroundImage = `linear-gradient(to right, ${newValue}, ${state[1]}`;
+        } else {
+            appElement.style.backgroundImage = `linear-gradient(to right, ${state[0]}, ${newValue}`;
+        }
     }
 })
